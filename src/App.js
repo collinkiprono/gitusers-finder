4438d8ec5e4d6b25bb9446b07b9b63528ca1acbf
glowupninja <one.kcollin@gmail.com>
2022-07-07 15:15:20 +0300

    Version 1.0 Github Finder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import User from "./components/users/User";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
-function App() {
+const App = () => {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+  //     {
+  //       headers: { "User-Agent": "glowupninja" },
+  //     }
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  //   // console.log(res.data);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <Fragment>
+            {/* <Navbar title="Github Finder" icon="fab fa-github"/> */}
+            <Navbar />
+
+            <div className="container">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
